04eb48a61ec5bdab5995b4f256cdf56e8b17c946
aerin <aerin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-23 20:17:24 +0000

    git-svn-id: https://plugins.svn.wordpress.org/quick-contact-form/trunk@522818 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- quick-contact-form-javascript.js
+++ quick-contact-form-javascript.js
@@ -1,13 +1,26 @@
-function clickclear(thisfield, defaulttext) {if (thisfield.value == defaulttext) {thisfield.value = ""; thisfield.style.color ="#465069;";}}
-function clickrecall(thisfield, defaulttext) {if (thisfield.value == "") {thisfield.value = defaulttext; thisfield.style.color ="#465069";}}
-
-
-
-
-
-
-
-
-
-
-
+function clickclear(thisfield, defaulttext) {if (thisfield.value == defaulttext) {thisfield.value = "";}}
+
+function clickrecall(thisfield, defaulttext) {if (thisfield.value == "") {thisfield.value = defaulttext;}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
