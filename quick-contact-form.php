0f308a86eb8ca40dabaadce0d168098b5cebf1cb
aerin <aerin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-25 18:33:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/quick-contact-form/trunk@523456 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- quick-contact-form.php
+++ quick-contact-form.php
@@ -1,30 +1,26 @@
 <?php
-
 /*
 Plugin Name: Quick Contact Form
-Plugin URI: http://www.aerin.co.uk/quick-contact-form/
-Description: A really, really simple contact form. There is nothing to configure, all you have to do is set up your email address and add the shortcode to your pages.
-Version: 1.1
+Plugin URI: http://www.aerin.co.uk/quick-contact-form-plugin
+Description: A really, really simple contact form. There is nothing to configure, just add your email address and it's ready to go.
+Version: 1.2
 Author: Graham Smith
 Author URI: http://www.aerin.co.uk
 */
 
 /*
 Copyright (C) 2012 Graham Smith, aerin.co.uk (graham AT aerin DOT co DOT uk)
-
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-add_shortcode('qcf', 'qcf_form');
+add_shortcode('qcf', 'qcf_loop');
 add_action('admin_init', 'qcf_init');
 add_action('admin_init', 'qcf_add_defaults');
 add_action('admin_menu', 'qcf_add_options_page');
 add_filter( 'plugin_action_links', 'qcf_plugin_action_links', 10, 2 );
 
 register_activation_hook(__FILE__, 'qcf_add_defaults');
-/*
-register_deactivation_hook( __FILE__, 'qcf_delete_options' );
-*/
+/* register_deactivation_hook( __FILE__, 'qcf_delete_options' ); */
 register_uninstall_hook(__FILE__, 'qcf_delete_options');
 
 $myStyleUrl = plugins_url('quick-contact-form-style.css', __FILE__);
@@ -32,21 +28,14 @@ wp_register_style('qcf_style', $myStyleUrl);
 wp_enqueue_style( 'qcf_style');
 
 $myScriptUrl = plugins_url('quick-contact-form-javascript.js', __FILE__);
-wp_register_script('nofuss_script', $myScriptUrl);
-wp_enqueue_script( 'nofuss_script');
+wp_register_script('qcf_script', $myScriptUrl);
+wp_enqueue_script( 'qcf_script');
 
 function qcf_add_defaults()
 	{
-	$qcf_options = array("Enquiry Form", "Complete the form below and we will be in contact very soon","Your Name", "Email Address","Message", "Send It!","","250","plain");
+	$qcf_options = array("Enquiry Form", "Complete the form below and we will be in contact very soon","Your Name", "Email Address","Message", "Send It!","","250","plain","","","","required","required","required","","","","","");
 	add_option('qcf_options', $qcf_options);
 	}
-
-function qcf_admin_notice()
-{
-if ($qcf_options[6] == "") echo '<div class="updated">
-       <p>For the <b>Easy Contact Form</b> to work you need to add your email address in the Settings page.</p>
-    </div>';
-}
 
 function qcf_delete_options()
 	{
@@ -60,7 +49,7 @@ function qcf_init()
 
 function qcf_add_options_page()
 	{
-	add_options_page('Quick Contact Form Options Page', 'Quick Contact', 'manage_options', __FILE__, 'qcf_options_page');
+	add_options_page('Contact Test Options Page', 'Contact Test', 'manage_options', __FILE__, 'qcf_options_page');
 	}
 
 function qcf_options_page()
@@ -69,20 +58,20 @@ function qcf_options_page()
 	<div style="margin-left: 14px">
 	<h1>The Quick Contact Form</h1>
 	<h2>Description</h2>
-	<p>This contact form plugin very simple and can be used almost straight out the box. All you need to do is add your email address, save and insert the shortcode into your posts and pages.
-	If you want to change the labels and captions just edit the existing entries and save. You can also alter the width of the form and change the border style.
-	When you save the changes the updated form will preview on the right.</p>
-		<h2>How to use the plugin</h2>
-		<p>To add the contact form your posts or pages use the short code: <code>[qcf]</code>.<br />
-		To use the form in a text widget add the line <code>add_filter('widget_text', 'do_shortcode');</code> to your functions.php file. You can now use the shortcode <code>[qcf]</code>.<br />
+	<p>This contact form plugin very simple and can be used almost straight out the box. All you need to do is add your email address, save and insert the shortcode into your posts and pages. If you want to change the labels and captions just edit the existing entries and save. You can also alter the width of the form and change the border style. When you save the changes the updated form will preview on the right.</p>
+	<h2>How to use the plugin</h2>
+	<p>To add the contact form your posts or pages use the short code: <code>[qcf]</code>.<br />
+	To use the form in a text widget add the line <code>add_filter('widget_text', 'do_shortcode');</code> to your functions.php file. You can now use the shortcode <code>[qcf]</code>.<br />
 	To add it to your theme files use <code>&lt;?php echo do_shortcode('[qcf]'); ?&gt;</code><br />
 	To change the styles use the plugin editor to fiddle with qcf-contact-form-styles.css</p>
-		<p>Not much else to add really. Enjoy using the plugin</p>
-		</div>
-		<div id="qcf-options">
-	<div id="qcf">
+	<p>Not much else to add really. Enjoy using the plugin</p>
+	</div>
+
+	<div id="qcf-options">
+	<div id="qcf-style">
 		<form method="post" action="options.php">
-		<?php settings_fields('my_qcf_options');
+		<?php
+		settings_fields('my_qcf_options');
 		$qcf_options = get_option('qcf_options');
 		$width = preg_replace("/[^0-9]/", "", $qcf_options[7]);
 		$width = $width.'px';
@@ -94,6 +83,9 @@ function qcf_options_page()
 		if ($qcf_options[8] == "plain") $plain = "checked"; else $plain = "";
 		if ($qcf_options[8] == "rounded") $rounded = "checked"; else $rounded = "";
 		if ($qcf_options[8] == "none") $none = "checked"; else $none = "";
+		if ($qcf_options[12] == "R2") $checked2 = "checked"; else $checked2 = "";
+		if ($qcf_options[13] == "R3") $checked3 = "checked"; else $checked3 = "";
+		if ($qcf_options[14] == "R4") $checked4 = "checked"; else $checked4 = "";
 		?>
 		<h2>Your email address</h2>	
 		<p><span style="color:red; font-weight: bold;">Important!</span> Enter YOUR email address below.  This won't display, it's just so the plugin knows where to send the message.</p>
@@ -103,12 +95,15 @@ function qcf_options_page()
 		<p>Form title (leave blank if you don't want a heading):</p>
 		<p><input type="text"  style="width:<?php echo $input; ?>;" name="qcf_options[0]" value="<?php echo $qcf_options[0]; ?>" /></p>
 		<p>This is the blurb that will appear below the heading and above the form (leave blank if you don't want any blurb):</p>
-		<p><input type="text"   style="width:100%" name="qcf_options[1]" value="<?php echo $qcf_options[1]; ?>" /></p>
+		<p><input type="text" style="width:100%" name="qcf_options[1]" value="<?php echo $qcf_options[1]; ?>" /></p>
 		<h2>Form fields and captions</h2>
-		<p>These can be any questions you like: name, email, telephone, shoe size, favourite colour and so on. The first two fields are required so don't leave them blank. The third field is a text area, you can leave this one blank if you like.</p><p>You can change the caption on the submit button as well.</p>
-		<p><input type="text"   style="width:<?php echo $input; ?>;" name="qcf_options[2]" value="<?php echo $qcf_options[2]; ?>" /></p>
-		<p><input type="text"   style="width:<?php echo $input; ?>;" name="qcf_options[3]" value="<?php echo $qcf_options[3]; ?>" /></p>
+		<p>These can be any questions you like: name, email, telephone, shoe size, favourite colour and so on. Tick the checkbox if the field is required.</p><p>You can change the caption on the submit button as well.</p>
+		<p><input type="text" style="width:<?php echo $input; ?>;" name="qcf_options[2]" value="<?php echo $qcf_options[2]; ?>" /></p>
+		<p><input type="checkbox" style="margin: 0;" name="qcf_options[12]" <?php echo $checked2; ?> value="R2">Required</p>
+		<p><input type="text" style="width:<?php echo $input; ?>;" name="qcf_options[3]" value="<?php echo $qcf_options[3]; ?>" /></p>
+		<p><input type="checkbox" style="margin: 0;" name="qcf_options[13]" <?php echo $checked3; ?> value="R3">Required</p>
 		<p><input type="text"  style="width:<?php echo $input; ?>;" name="qcf_options[4]" value="<?php echo $qcf_options[4]; ?>" /></p>
+		<p><input type="checkbox" style="margin: 0;" name="qcf_options[14]" <?php echo $checked4; ?> value="R4">Required</p>
 		<p><input type="text" id="submit" style="width:<?php echo $input; ?>; font-size: 130%;cursor:auto; color: #FFF" name="qcf_options[5]" value="<?php echo $qcf_options[5]; ?>" /></p>
 		<h2>Form Width</h2>
 		<p>Enter the width of the form in pixels. Just enter the value, no need to add 'px'. The current width is as you see it here.</p>
@@ -126,18 +121,29 @@ function qcf_options_page()
 		</form>
 		</div>
 		</div>
-	<div id="qcf-options">
-
+		<div id="qcf-options">
 		<h2>Form Preview</h2>
-		<p>Note: When you add the form to your site it uses the theme styles so it won't look exactly like the one below.</p>
+		<p>Note: The form uses the theme styles so yours won't look exactly like the one below.</p>
 		<?php
-		qcf_theform("options_page");
 		if (isset($_POST['submit']))
-		qcf_verify("options_page");
+		{
+		$formvalues = $_POST;
+		$formerrors = array();
+       		if (!qcf_verify_form($formvalues, $formerrors)) qcf_display_form($formvalues, $formerrors, 0);
+    		else qcf_process_form($formvalues);
+		}
+		else
+		{
+		$qcf_options['qcfname2'] = $qcf_options[2];
+		$qcf_options['qcfname3'] = $qcf_options[3];
+		$qcf_options['qcfname4'] = $qcf_options[4];
+		qcf_display_form($qcf_options, null,0);
+		}
 		?>
 		</div>
+
 <?php
-}
+	}
 
 function qcf_plugin_action_links( $links, $file )
 	{
@@ -149,87 +155,108 @@ function qcf_plugin_action_links( $links, $file )
 	return $links;
 	}
 
-function qcf_theform($options)
+function qcf_verify_form(&$values, &$errors)
+	{
+	$qcf_options = get_option('qcf_options');
+	$values['qcfname2'] = preg_replace("/[^a-zA-Z0-9.\s]/", "", $values['qcfname2']);
+	$values['qcfname3'] = preg_replace("/[^a-zA-Z0-9@._\-\s]/", "", $values['qcfname3']);
+	$values['qcfname4'] = preg_replace("/[^a-zA-Z0-9.,\?\s]/", "", $values['qcfname4']);
+	
+	if ($qcf_options[12] == "R2"){
+	if (empty($values['qcfname2']) || $values['qcfname2'] == $qcf_options[2]) $errors['qcfname2'] = 'Giving me '.strtolower($qcf_options[2]).' would really help';}
+	if ($qcf_options[13] == "R3"){
+	if (empty($values['qcfname3']) || $values['qcfname3'] == $qcf_options[3]) $errors['qcfname3'] = 'The '.strtolower($qcf_options[3]).' is missing';}
+	if ($qcf_options[14] == "R4"){
+	if (empty($values['qcfname4']) || $values['qcfname4'] == $qcf_options[4]) $errors['qcfname4'] = 'What is the '.strtolower($qcf_options[4]).'?';}
+	return (count($errors) == 0);
+	}
+
+function qcf_display_form($values, $errors, $whichpage)
 	{
 	$qcf_options = get_option('qcf_options');
 	$width = preg_replace("/[^0-9]/", "", $qcf_options[7]);
 	if ($qcf_options[8] == "none") $padding = 0; else $padding = 12;
-	if ($options == "options_page")
-	{
-	$input = $width;
-	$submit = $width;
-	$textarea = $width;
-	}
-	else
-	{
 	$input = $width - $padding;
 	$textarea = $width - $padding;
-	$submit = $width + 14 - $padding;
-	}
+	$submit = $width + $whichpage - $padding;
 	$width = $width.'px';
 	$input =  $input.'px';
 	$textarea = $textarea.'px';
 	$submit = $submit.'px';
 	$border = $qcf_options[8];
+	if ($qcf_options[12] == "R2") $required2 = 'class="required"';
+	if ($qcf_options[13] == "R3") $required3 = 'class="required"';
+	if ($qcf_options[14] == "R4") $required4 = 'class="required"';
 	?>
-	<div id="qcf">
+	<div id="qcf-style">
 	<div id="<?php echo $border; ?>" style="width: <?php echo $width; ?>">
-	<h2><?php echo $qcf_options[0]; ?></h2>
-	<p><?php echo $qcf_options[1]; ?></p>
-	<form action="" method="POST">
-	<div>
-	<input type="text"  style="width:<?php echo $input; ?>" label="Name" name="qcfname"  value="<?php echo $qcf_options[2]; ?>" onfocus="clickclear(this, '<?php echo $qcf_options[2]; ?>')" onblur="clickrecall(this,'<?php echo $qcf_options[2]; ?>')"></div>
-	<div>
-	<input type="text"  style="width:<?php echo $input; ?>" label="Callback" name="qcfcontact"  value="<?php echo $qcf_options[3]; ?>" onfocus="clickclear(this, '<?php echo $qcf_options[3]; ?>')" onblur="clickrecall(this,'<?php echo $qcf_options[3]; ?>')"></div>
-	<textarea name="qcfmessage" label="Message" rows="6" style="width:<?php echo $textarea ?>" onFocus="this.value=''; this.onfocus=null;"><?php echo $qcf_options[4] ?></textarea>
-	<div><input type="submit" id="submit" style="width:<?php echo $submit; ?>" name="submit" value="<?php echo $qcf_options[5]; ?>">	
-	</div>
-	</form>
-	</div>
-	</div>
 	<?php
-	}
+	if (count($errors) > 0)
+		echo "<h2>Oops, got a few problems here</h2><p class='error'>Can you sort out the details highlighted below.</p>";
+	else
+		echo '<h2>'.$qcf_options[0].'</h2><p>'. $qcf_options[1].'</p>';
+?>
+	<form action="" method="POST">
+	<p class="error"><?= $errors['qcfname2'] ?></p>
+	<p><input type="text" <?php echo $required2; ?> style="width:<?php echo $input; ?>" label="Name" name="qcfname2"  value="<?php echo $values['qcfname2']; ?>" onfocus="clickclear(this, '<?php echo $values['qcfname2']; ?>')" onblur="clickrecall(this,'<?php echo $values['qcfname2']; ?>')"></p>
+	<p class="error"><?= $errors['qcfname3'] ?></p>
+	<p><input type="text" <?php echo $required3; ?> style="width:<?php echo $input; ?>" label="Contact" name="qcfname3"  value="<?php echo $values['qcfname3']; ?>" onfocus="clickclear(this, '<?php echo $values['qcfname3']; ?>')" onblur="clickrecall(this,'<?php echo $values['qcfname3']; ?>')"></p>
+	<p class="error"><?= $errors['qcfname4'] ?></p>
+	<p><textarea  <?php echo $required4; ?> name="qcfname4" label="Message" rows="6" style="width:<?php echo $textarea ?>" onFocus="this.value=''; this.onfocus=null;"><?php echo $values['qcfname4'] ?></textarea></p>
+	<p><input type="submit" id="submit" style="width:<?php echo $submit; ?>" name="submit" value="<?php echo $qcf_options[5]; ?>">	
+	</form>
+	</div></div>
+<?php
+}
 
-function qcf_verify()
+function qcf_process_form($values)
 	{
 	$qcf_options = get_option('qcf_options');
-	$form_valid = true;
-	$qcf_name = preg_replace("/[^a-zA-Z0-9@.\+\-_\s]/", "", $_REQUEST['qcfname']);
-	$qcf_contact = preg_replace("/[^a-zA-Z0-9@.\+\-_\s]/", "", $_REQUEST['qcfcontact']);
-	$qcf_message = preg_replace("/[^a-zA-Z0-9@.\+\-_\s]/", "", $_REQUEST['qcfmessage']);
 	$qcf_sendmail = $qcf_options[6];
 	$ip=$_SERVER['REMOTE_ADDR'];
 	$url = $_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"];
-   	if (empty($qcf_contact) || $qcf_contact== $qcf_options[3]) { $error = "Please enter ".strtolower($qcf_options[3]); $form_valid = false; }
-	if (empty($qcf_name) || $qcf_name == $qcf_options[2]) { $error = "Please enter ".strtolower($qcf_options[2]); $form_valid = false; }
-	if (empty($qcf_sendmail) && $options == 'options_page') { $error = 'You need to set up an email address first';}
-	if ($form_valid)
+	$subject = "Enquiry from {$values['qcfname2']}";
+	$headers = "From: {$values['qcfname2']}<{$values['qcfname3']}>\r\n";
+	$headers .= "MIME-Version: 1.0\r\n";
+   	$headers .= "Content-Type: text/html; charset=\"utf-8\"\r\n"; 
+	$message = "<html><h2>The message was:</h2>";
+	$message .= "<p><b>{$qcf_options[2]}: </b>{$values['qcfname2']}</p>";
+	$message .= "<p><b>{$qcf_options[3]}: </b>{$values['qcfname3']}</p>";
+	$message .= "<p><b>{$qcf_options[4]}: </b>{$values['qcfname4']}</p>";
+	$message .= "<p>The message was sent from this page: <b>$url</b></p>";
+	$message .= "<p>This is the senders IP address: <b>$ip</b></p></html>";
+	$message .= "</html>"; 
+
+	mail($qcf_sendmail, $subject, $message, $headers);
+
+	echo '<div id="qcf-style"><div id="'.$qcf_options[8].'" style="width:'.$qcf_options[7].'px;">
+	<h2>Message Sent!</h2>
+	<p>Thank you for your enquiry. I&#146;ll be in contact soon</p>
+	<p>The details you sent me were:</p>
+	<p><b>'.$qcf_options[2].': </b><br />'.$values['qcfname2'].'</p>
+	<p><b>'.$qcf_options[3].': </b><br />'.$values['qcfname3'].'</p>
+	<p><b>'.$qcf_options[4].': </b><br />'.$values['qcfname4'].'</p></div></div>';  	
+	}
+
+function qcf_loop()
+{
+	ob_start();	
+	if (isset($_POST['submit']))
 		{
-		$subject = "Enquiry from $qcf_name";
-		$headers = "From: $qcf_name <$qcf_contact>\r\n";
-		$headers .= "MIME-Version: 1.0\r\n";
-   		$headers .= "Content-Type: text/html; charset=\"utf-8\"\r\n";
-		$message = "<html><p>$qcf_options[2]: <b>$qcf_name</b></p>";
-		$message .= "<p>$qcf_options[3]: <b>$qcf_contact</b></p>";
-		$message .= "<p>$qcf_options[4]: <b>$qcf_message</b></p>";
-		$message .= "<p>The message was sent from this page: <b>$url</b></p>";
-		$message .= "<p>This is the senders IP address: <b>$ip</b></p></html>";
-		mail($qcf_sendmail, $subject, $message, $headers);
-		echo '<div id="qcf-message"><p>Thank you for your enquiry '.$qcf_name.'</p><p><a href="javascript:history.go(-1)">Click to close</a></p></div>';	
+		$formvalues = $_POST;
+		$formerrors = array();
+		if (!qcf_verify_form($formvalues, $formerrors)) qcf_display_form($formvalues, $formerrors, 14);
+		else qcf_process_form($formvalues);
 		}
 	else
 		{
-		echo '<div id="qcf-message"><p>'.$error.'</p><p><a href="javascript:history.go(-1)">Return to the  form</a></p></div>';	
+		$qcf_options = get_option('qcf_options');
+		$qcf_options['qcfname2'] = $qcf_options[2];
+		$qcf_options['qcfname3'] = $qcf_options[3];
+		$qcf_options['qcfname4'] = $qcf_options[4];
+		qcf_display_form($qcf_options, null,14);
 		}
-	}
-
-function qcf_form()
-	{
-	ob_start();
-	qcf_theform("");
-	if (isset($_POST['submit']))
-	qcf_verify();
 	$output_string=ob_get_contents();;
 	ob_end_clean();
 	return $output_string;
-	}
+}
--- readme.txt
+++ readme.txt
@@ -1,71 +1,76 @@
-=== Quick Contact Form ===
-
-Contributors: 
-Tags: contact form
-Requires at least: 2.7
-Tested up to: 3.3.1
-Stable tag: trunk
-
-Simple plug and play contact form. 
-
-== Description ==
-
-A really, really simple contact form. There is nothing to configure, all you have to do is set up your email address and add the shortcode to your pages.
-
-= Features =
-
-*	Ready made contact form with three fields.
-*	Compact and easy to use.
-*	No configuration required.
-*	Editable title, blurb, labels and captions (if you want to).
-*	Pre-set range of border styles.
-*	Optional form width adjustment.
-*	Safe for use with animaals and children.
-
-= Developers plugin page =
-
-[callback form plugin](http://aerin.co.uk/quick-contact-form/).
-
-== Installation ==
-
-1.	Download the plugin.
-2.	Login to your wordpress dashboard.
-3.	Go to 'Plugins', 'Add New' then 'Upload'.
-4.	Browse to the downloaded plugin then then 'Install Now'.
-5.	Activate the plugin.
-6.	Go to the plugin 'Settings' page to set the recipient's email address.
-7.	Edit the field labels and submit button caption is required.
-8.	To use the form in your posts and pages add the shortcode [qcf].
-9.	To use the form in a text widget add the line `add_filter('widget_text', 'do_shortcode');` to your functions.php file. You can now use the shortcode [qcf].
-10.	To use the form in your theme files use the code `<?php echo do_shortcode('[qcf]'); ?>`.
-
-== Frequently Asked Questions ==
-
-= How do I change the labels and captions? =
-Go to your plugin list and scroll down until you see the 'Quick Contact Form' and click on 'Settings'. Change the labels and settings and click on 'Save Settings'.
-
-= What's the shortcode? =
-[qcf]
-
-= How do I change the colours? =
-Go to your plugin list and scroll down until you see the 'Quick Contact Form' and click on 'Edit'. Click on the link 'quick-contact-form-styles.css' over on the right. Make the changes and click on 'Update Changes' down the bottom.
-
-= Can I add more fields? =
-No.
-
-= Why not? =
-Well OK yes you can add more fields if you want but you are going to have to fiddle about with the php file which needs a bit of care an attention. Everything you need to know is in the [wordpress codex](http://codex.wordpress.org/Writing_a_Plugin).
-
-= It's all gone wrong! =
-If it all goes wrong, just reinstall the plugin and start again. If you need help then [contact me](http://aerin.co.uk/contact-me/).
-
-= Does anybody read these things? =
-I did, and so have you.  Maybe there will be more, who knows.....
-
-== Changelog ==
-
-= 1.1 =
-*	Fixed 'onclick' bug
-
-= 1.0 =
+=== Quick Contact Form ===
+
+Contributors: 
+Tags: contact form
+Requires at least: 2.7
+Tested up to: 3.3.1
+Stable tag: trunk
+
+Simple plug and play contact form. 
+
+== Description ==
+
+A really, really simple contact form. There is nothing to configure, all you have to do is set up your email address and add the shortcode to your pages.
+
+= Features =
+
+*	Ready made contact form with three fields.
+*	Compact and easy to use.
+*	No configuration required.
+*	Editable title, blurb, labels and captions (if you want to).
+*	Contextual validation
+*	Pre-set range of border styles.
+*	Optional form width adjustment.
+*	Safe for use with animaas and children.
+
+= Developers plugin page =
+
+[callback form plugin](http://aerin.co.uk/quick-contact-form/).
+
+== Installation ==
+
+1.	Download the plugin.
+2.	Login to your wordpress dashboard.
+3.	Go to 'Plugins', 'Add New' then 'Upload'.
+4.	Browse to the downloaded plugin then then 'Install Now'.
+5.	Activate the plugin.
+6.	Go to the plugin 'Settings' page to set the recipient's email address.
+7.	Edit the field labels and submit button caption is required.
+8.	To use the form in your posts and pages add the shortcode [qcf].
+9.	To use the form in a text widget add the line `add_filter('widget_text', 'do_shortcode');` to your functions.php file. You can now use the shortcode [qcf].
+10.	To use the form in your theme files use the code `<?php echo do_shortcode('[qcf]'); ?>`.
+
+== Frequently Asked Questions ==
+
+= How do I change the labels and captions? =
+Go to your plugin list and scroll down until you see the 'Quick Contact Form' and click on 'Settings'. Change the labels and settings and click on 'Save Settings'.
+
+= What's the shortcode? =
+[qcf]
+
+= How do I change the colours? =
+Go to your plugin list and scroll down until you see the 'Quick Contact Form' and click on 'Edit'. Click on the link 'quick-contact-form-styles.css' over on the right. Make the changes and click on 'Update Changes' down the bottom.
+
+= Can I add more fields? =
+No.
+
+= Why not? =
+Well OK yes you can add more fields if you want but you are going to have to fiddle about with the php file which needs a bit of care an attention. Everything you need to know is in the [wordpress codex](http://codex.wordpress.org/Writing_a_Plugin).
+
+= It's all gone wrong! =
+If it all goes wrong, just reinstall the plugin and start again. If you need help then [contact me](http://aerin.co.uk/contact-me/).
+
+= Does anybody read these things? =
+I did, and so have you.  Maybe there will be more, who knows.....
+
+== Changelog ==
+
+= 1.2 =
+*	Changed the way error and the thank you messages are displayed (no more popups).
+*	Added option to select which fields are required.
+
+= 1.1 =
+*	Fixed 'onclick' bug
+
+= 1.0 =
 *	Initial Issue
